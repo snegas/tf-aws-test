688569aef1f55d8035266827783a38803267f71d
Vitaliy Minayev <Vitaliy_Minayev@epam.com>
2021-02-14 19:31:20 +0200

    add iam related resources

---
+++ iam.tf
@@ -0,0 +1,72 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_task_in_policy" {
+  name = "ecs_ssm_policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+  description = "policy to allow access by ECS task to secret from SSM"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "secretsmanager:GetSecretValue"
+      ],
+      "Resource": [
+        "${aws_secretsmanager_secret.secret_rds.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+#resource "aws_iam_role_policy_attachment" "task_ssm" {
+#  role       = "${aws_iam_role.ecs_task_role.name}"
+#  policy_arn = "${aws_iam_policy.ecs_task_in_policy.arn}"
+#}
+
