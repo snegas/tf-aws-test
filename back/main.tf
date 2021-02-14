fee4f512df07fa71b932ae7844da8768f13a5f81
Vitaliy Minayev <Vitaliy_Minayev@epam.com>
2021-02-14 19:33:35 +0200

    add remote backend support (s3)

---
+++ back/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "test-tf"  # Enable versioning so we can see 
+  # the full revision history of ourstate files
+  versioning {
+    enabled = true
+  }  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
